86ef5a9273c5c97671e0adf6ca0824d5c4aeb7c4
sekema954 <sekema954@gmail.com>
2024-09-04 16:04:52 -0400

    edited

--- client/src/Pages/SearchCompaniesPage.jsx
+++ client/src/Pages/SearchCompaniesPage.jsx
@@ -17,7 +17,7 @@ function SearchCompaniesPage() {
       method: 'GET',
       headers: {
         'x-rapidapi-key': process.env.REACT_APP_API_KEY,
-        'x-rapidapi-host': process.env.REACT_APP_API_HOST,
+        'x-rapidapi-host': process.env.REACT_APP_API_HOST
       },
     };
 
